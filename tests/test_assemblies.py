ee300d1ba42b89a32456a18cdddbda43a9931ca6
Jonathan Prescott-Roy <jjpr@mit.edu>
2020-11-09 13:25:02 -0500

    Pull out cruft from test_assemblies.

--- tests/test_assemblies.py
+++ tests/test_assemblies.py
@@ -1,36 +1,6 @@
 import pytest
-import string
-import unicodedata
-
-from xarray import DataArray
 
 from brainio_base.assemblies import DataAssembly
-
-dim_set = list(string.ascii_lowercase)
-
-coord_set = {
-  "greek": [unicodedata.name(chr(code)).split()[3].lower() for code in range(945, 970)],
-  "colors": ["red", "green", "blue", "purple", "yellow", "orange"],
-  "compass": ["north", "south", "east", "west", "northeast", "southeast", "southwest", "northwest"],
-  "integer": list(range(100)),
-}
-
-
-def rect(x, y):
-  return [list(range(j*y, j*y+y)) for j in range(x)]
-
-
-def da_gen(x, y):
-  return DataArray(
-    data=rect(x, y),
-    coords={
-      list(coord_set)[0]: (dim_set[0], coord_set[list(coord_set)[0]][:x]),
-      list(coord_set)[1]: (dim_set[0], coord_set[list(coord_set)[1]][:x]),
-      list(coord_set)[2]: (dim_set[1], coord_set[list(coord_set)[2]][:y]),
-      list(coord_set)[3]: (dim_set[1], coord_set[list(coord_set)[3]][:y]),
-    },
-    dims=(dim_set[0], dim_set[1])
-  )
 
 class TestIndex:
     def test_single_element(self):
@@ -57,26 +27,38 @@ class TestIndex:
 
 
 class TestMultiGroupby:
-    # @pytest.mark.skip(reason="Skip until https://github.com/pydata/xarray/issues/3717 is fixed.")
     def test_single_dimension(self):
         d = DataAssembly([[1, 2, 3], [4, 5, 6]], coords={'a': ['a', 'b'], 'b': ['x', 'y', 'z']}, dims=['a', 'b'])
         g = d.multi_groupby(['a']).mean(...)
         assert g.equals(DataAssembly([2, 5], coords={'a': ['a', 'b']}, dims=['a']))
 
-    # @pytest.mark.skip(reason="Skip until https://github.com/pydata/xarray/issues/3717 is fixed.")
     def test_single_dimension_int(self):
         d = DataAssembly([[1, 2, 3], [4, 5, 6]], coords={'a': [1, 2], 'b': [3, 4, 5]}, dims=['a', 'b'])
         g = d.multi_groupby(['a']).mean(...)
         assert g.equals(DataAssembly([2., 5.], coords={'a': [1, 2]}, dims=['a']))
 
-    @pytest.mark.skip(reason="Skip until https://github.com/pydata/xarray/issues/3717 is fixed.")
     def test_single_coord(self):
-        d = DataAssembly(da_gen(3, 7))
+        d = DataAssembly(
+            data=[
+                [ 0,  1,  2,  3,  4,  5,  6],
+                [ 7,  8,  9, 10, 11, 12, 13],
+                [14, 15, 16, 17, 18, 19, 20]
+            ],
+            coords={
+                "greek": ("a", ['alpha', 'beta', 'gamma']),
+                "colors": ("a", ['red', 'green', 'blue']),
+                "compass": ("b", ['north', 'south', 'east', 'west', 'northeast', 'southeast', 'southwest']),
+                "integer": ("b", [0, 1, 2, 3, 4, 5, 6]),
+            },
+            dims=("a", "b")
+        )
         g = d.multi_groupby(['greek']).mean(...)
-        assert g.equals(DataAssembly([[3], [10], [17]], coords={'greek': ('a', ['alpha', 'beta', 'gamma'])}, dims=['a', 'b']))
-        # ideally, we would want `g.equals(DataAssembly([2, 5],
-        #   coords={'a': ('multi_dim', ['a', 'b']), 'b': ('multi_dim', ['c', 'c'])}, dims=['multi_dim']))`
-        # but this is fine for now.
+        c = DataAssembly(
+            data=[3, 10, 17],
+            coords={'greek': ('greek', ['alpha', 'beta', 'gamma'])},
+            dims=['greek']
+        )
+        assert g.equals(c)
 
     def test_single_dim_multi_coord(self):
         d = DataAssembly([1, 2, 3, 4, 5, 6],
